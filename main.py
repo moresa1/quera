5ca2f04e065b9b0b16604c18806900d87936ef08
Moh Rez <rezmoh7@gmail.com>
2021-01-14 10:52:42 +0330

    fibu :|

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-user_input = sum(list(map(lambda x: 181 if int(x) == 0 else int(x) , input().split())))
-print('Yes' if user_input == 180 else 'No')
+user_input = int(input())
+
+for _ in range(user_input, 0, -1):
+	for __ in range(_, 0, -1):
+		print(_ * __, end=' ')
+	print()
