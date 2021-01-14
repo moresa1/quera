2357b8e7ce1380794bfedc0a6b442b858986e7fb
Moh Rez <rezmoh7@gmail.com>
2021-01-14 12:07:38 +0330

    khar da chaman :(

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Payin Barare' if int(input()) % 2 == 1 else 'Bala Barare')
+a, b, l = list(map(lambda x: int(x), input().split()))
+
+all_l = 0
+all_s = 0
+c = 0
+
+while True:
+	# if l == 1:
+	# 	all_s = a
+	# 	break
+	c += 1
+	all_l += c
+	if all_l > l:
+		break
+	all_s += c * a + b
+
+print(all_s)
